7c03e0f5d2f339fc5a56d13297abbc09bd7c6a62
Tien Minh Nguyen <tiennguyen@simpsonmfg.com>
2024-11-11 14:46:04 +0700

    Core React Concept - JSX, Hook, Mapping

---
+++ week-4/src/SearchParams.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
+const SearchParams = () => {
+    const [location, setLocation] = useState("");
+    const [animal, setAnimal] = useState("");
+    
+    return (
+        <div className="search-params">
+            <form>
+                <label htmlFor="location">
+                    Location
+                    <input onChange={ (e) => setLocation(e.target.value)} id="location" value={location} placeholder="Location" />
+                </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select 
+                        onChange={ e => setAnimal(e.target.value)} 
+                        id="animal" 
+                        value={animal}>
+                        <option />
+                        {ANIMALS.map((animal) => (
+                            <option key={animal}>{animal}</option>
+                        ))}
+                    </select>
+                </label>
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default SearchParams;
